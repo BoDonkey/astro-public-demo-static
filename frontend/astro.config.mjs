c03b134a60d7e3ba924467d6176320faf6f325a8
Robert Means <robert@robertmeans.net>
2026-03-11 15:05:24 -0400

    Read aposHost and host from environment variables

--- frontend/astro.config.mjs
+++ frontend/astro.config.mjs
@@ -11,12 +11,12 @@ export default defineConfig({
   server: {
     port: process.env.PORT ? parseInt(process.env.PORT) : 4321,
     // Required for some hosting, like Heroku
-    // host: true
+    host: process.env.HOST || false
   },
   adapter: isStatic ? undefined : node({ mode: 'standalone' }),
   integrations: [
     apostrophe({
-      aposHost: 'http://localhost:3000',
+      aposHost: process.env.APOS_HOST || 'http://localhost:3000',
       widgetsMapping: './src/widgets',
       templatesMapping: './src/templates',
       includeResponseHeaders: [
